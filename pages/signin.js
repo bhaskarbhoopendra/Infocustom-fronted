7fd6b12b1d20e642482e8cda72e57dc3c8edda9f
Bhoopendra <bhaskarbhoopendra8@gmail.com>
2019-10-23 13:17:45 +0530

    forst commit

---
+++ pages/signin.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const Signin = ()=>{
+   return <Layout>
+       <h2>Signin Page</h2>
+       <Link href="/">Index page</Link>
+   </Layout>
+}
+
+export default Signin;
---
+++ pages/signup.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+const Signup = ()=>{
+   return <Layout>
+       <h2>Signup page</h2>
+   </Layout>
+}
+
+export default Signup;
