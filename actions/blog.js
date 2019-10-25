5fd2cfbb9838b71e6908a651b68ea5cb5eb2fe30
Bhoopendra <bhaskarbhoopendra8@gmail.com>
2019-10-25 20:13:13 +0530

    adding rich text editor

--- actions/blog.js
+++ actions/blog.js
@@ -1,14 +1,44 @@
-import fetch from 'isomorphic-fetch';
-import { API } from '../config';
+import fetch from "isomorphic-fetch";
+import { API } from "../config";
 
 export const createBlog = (blog, token) => {
-    return fetch(`${API}/blog`, {
+  return fetch(`${API}/blog`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: blog
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+
+export const listBlogsWithCategoriesAndTags = (skip, limit) => {
+    const data = {
+        limit,
+        skip
+    };
+    return fetch(`${API}/blogs-categories-tags`, {
         method: 'POST',
         headers: {
             Accept: 'application/json',
-            Authorization: `Bearer ${token}`
+            'Content-Type': 'application/json'
         },
-        body: blog
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const singleBlog = slug => {
+    return fetch(`${API}/blog/${slug}`, {
+        method: 'GET'
     })
         .then(response => {
             return response.json();
--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,9 @@
-import {useState} from 'react';
-import {APP_NAME} from '../config';
-import Link from 'next/link';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import {isAuth,signout} from '../actions/auth';
+import { useState } from "react";
+import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { APP_NAME } from "../config";
+import { signout, isAuth } from "../actions/auth";
 import {
   Collapse,
   Navbar,
@@ -15,52 +15,84 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  DropdownItem
+} from "reactstrap";
+import ".././node_modules/nprogress/nprogress.css";
 
-  Router.onRouteChangeStart = url=> NProgress.start();
-  Router.onRouteChangeComplete = url=> NProgress.done();
-  Router.onRouteChangeError = url=> NProgress.done();
-  
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
-  const Header = ()=>{
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
-    return (
-        <div>
-          <Navbar color="light" light expand="md">
-            <Link href="/"><NavLink className="font-weight-bold">{APP_NAME}</NavLink></Link>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                {!isAuth() && <React.Fragment>
-                  <NavItem>
-                  <Link href="/signup"><NavLink>Signup</NavLink></Link>
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <Link href="/">
+          <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
+        </Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <React.Fragment>
+              <NavItem>
+                <Link href="/blogs">
+                  <NavLink>Blogs</NavLink>
+                </Link>
+              </NavItem>
+            </React.Fragment>
+
+            {!isAuth() && (
+              <React.Fragment>
+                <NavItem>
+                  <Link href="/signin">
+                    <NavLink>Signin</NavLink>
+                  </Link>
                 </NavItem>
                 <NavItem>
-                  <Link href="/signin"><NavLink>Signin</NavLink></Link>
-                </NavItem>    
-                </React.Fragment>}                  
+                  <Link href="/signup">
+                    <NavLink>Signup</NavLink>
+                  </Link>
+                </NavItem>
+              </React.Fragment>
+            )}
 
-          {isAuth()&&  isAuth().role===0&&  ( 
-                <NavItem>                
-                    <Link href="/user"><NavLink>{`${isAuth().name}'s Dashboard`}</NavLink></Link>                
-                </NavItem>  )}  
+            {isAuth() && isAuth().role === 0 && (
+              <NavItem>
+                <Link href="/user">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
 
-                {isAuth()&&  isAuth().role===1&&( 
-                 <NavItem>                
-                    <Link href="/admin"><NavLink>{`${isAuth().name}'s Dashboard`}</NavLink></Link>                
-                 </NavItem>  )}  
+            {isAuth() && isAuth().role === 1 && (
+              <NavItem>
+                <Link href="/admin">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
 
-                {isAuth()&&( 
-                 <NavItem>                
-                    <NavLink style={{cursor:'pointer'}} onClick={()=>signout(()=>{Router.replace(`/signin`)})}>Signout</NavLink>                
-                 </NavItem>  )}  
-
-              </Nav>
-            </Collapse>
-          </Navbar>
-        </div>
-      );
-  }
+            {isAuth() && (
+              <NavItem>
+                <NavLink
+                  style={{ cursor: "pointer" }}
+                  onClick={() => signout(() => Router.replace(`/signin`))}
+                >
+                  Signout
+                </NavLink>
+              </NavItem>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 export default Header;
